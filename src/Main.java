0137595e840b1edebc8ebc13b6befe9ba9633389
Антон <antonolegovich2017@icloud.com>
2022-07-31 17:58:14 +0600

    domashka uslovnii operator

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS == 0) {
             System.out.println(" Установиет версию приложения для IOS по ссылке");
-        } else if (clientOS == 1)  {
-                System.out.println(" Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println(" Установите версию приложения для Android по ссылке");
         } else {
-            System.out.println(" операционна система не найдена");}
+            System.out.println(" операционна система не найдена");
+        }
 
         // Задание 2
-        int clientDeviceYear = 2020;
-        if (clientDeviceYear <= 2015 && clientOS==1) {
+        int clientDeviceYear = 2010;
+        if (clientDeviceYear <= 2015 && clientOS == 1) {
             System.out.println("установите облегченную версию приложения для Андройд по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOS==0) {
+        } else if (clientDeviceYear <= 2015 && clientOS == 0) {
             System.out.println(" установите облегченную версию для IOS по ссылке");
-        } else if (clientDeviceYear > 2015 && clientOS==1) {
+        } else if (clientDeviceYear > 2015 && clientOS == 1) {
             System.out.println("установите обычную версию приложения для Андроид");
-        } else if (clientDeviceYear>2015 && clientOS==0) {
+        } else if (clientDeviceYear > 2015 && clientOS == 0) {
             System.out.println("установите обычную версию приложения для IOS");
         }
         // Задание 3
+        int year = 2016;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(" Год является всокосным");
+        } else {
+            System.out.println(" год не является высокосным");
+        }
+        // Задание 4
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println(" доставка занимает сутки");
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 100) {
+            System.out.println(" доставка занимает двое суток");
+        }
 
 
     }
