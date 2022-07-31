a40382fea7b98fffd42bb9140f354e1d5a112247
Антон <antonolegovich2017@icloud.com>
2022-07-31 17:06:32 +0600

    domashka uslovnii operator

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        int clientOS = 0;
+        int clientOS = 1;
         if (clientOS == 0) {
             System.out.println(" Установиет версию приложения для IOS по ссылке");
-        } if (clientOS == 1)  {
+        } else if (clientOS == 1)  {
                 System.out.println(" Установите версию приложения для Android по ссылке");
-        } // Задание 2
-        int clientOs = 1;
+        } else {
+            System.out.println(" операционна система не найдена");}
+
+        // Задание 2
         int clientDeviceYear = 2020;
-        if (clientDeviceYear >= 2015) {
-            System.out.println();
+        if (clientDeviceYear <= 2015 && clientOS==1) {
+            System.out.println("установите облегченную версию приложения для Андройд по ссылке");
+        } else if (clientDeviceYear <= 2015 && clientOS==0) {
+            System.out.println(" установите облегченную версию для IOS по ссылке");
+        } else if (clientDeviceYear > 2015 && clientOS==1) {
+            System.out.println("установите обычную версию приложения для Андроид");
+        } else if (clientDeviceYear>2015 && clientOS==0) {
+            System.out.println("установите обычную версию приложения для IOS");
         }
+        // Задание 3
+
 
     }
 }
