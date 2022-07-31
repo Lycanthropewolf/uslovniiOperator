20d5943361918c647e494d0e0874a780ecba6514
Антон <antonolegovich2017@icloud.com>
2022-07-31 15:54:38 +0600

    domashka uslovnii operator

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
             System.out.println(" Установиет версию приложения для IOS по ссылке");
         } if (clientOS == 1)  {
                 System.out.println(" Установите версию приложения для Android по ссылке");
-            }
+        } // Задание 2
+        int clientOs = 1;
+        int clientDeviceYear = 2020;
+        if (clientDeviceYear >= 2015) {
+            System.out.println();
         }
 
-
     }
+}
